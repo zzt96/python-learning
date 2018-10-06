1b3ebf2bc874db4acd3a49daefe07607c3d6bb8c
郑润明 <zhengrunming@zhengrunmingdeMacBook-Pro.local>
2018-10-06 22:58:37 +0800

    10.06

--- func_test.py
+++ func_test.py
@@ -1,4 +1,6 @@
 from def_quadratic import quadratic
 from func_multi_multiplication import product
+from hanoi import move
 # print('quadratic(2, 3, 1) =', quadratic('a','b', 1))
-print('product(2, 3, 1) =', product(2, 3, 1))
+# print('product(2, 3, 1) =', product(2, 3, 1))
+move(4, 'A', 'B', 'C')
