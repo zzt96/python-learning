17afb1205dcb1a8c2aaae96d96534bd5fdb052af
zzt96 <136901341@qq.com>
2018-10-07 17:55:16 +0800

    x

--- func_test.py
+++ func_test.py
@@ -3,4 +3,4 @@ from func_multi_multiplication import product
 from hanoi import move
 # print('quadratic(2, 3, 1) =', quadratic('a','b', 1))
 # print('product(2, 3, 1) =', product(2, 3, 1))
-move(4, 'A', 'B', 'C')
+move(3, 'A', 'B', 'C')
